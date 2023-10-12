4ba2a57045430ae0d40dd9c80a3aa55e2a550f1c
jose cuela <jgcuelap@gmail.com>
2023-10-12 04:47:26 -0500

    hotfix: update redirect home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'commits', pathMatch: 'full' },
   {
-    path: '',
+    path: 'commits',
     component: HomeComponent,
+  },
+  {
+    path: 'home',
+    component: LandingComponent,
   },
 ];
 
