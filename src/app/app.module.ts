b1dc28b5b26c5d3d2984ef309894ecac0ee9ded0
jose cuela <jgcuelap@gmail.com>
2023-10-12 04:48:09 -0500

    chore: indent app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TableComponent } from './home/components/table/table.component';
+import { TabsComponent } from './home/components/tabs/tabs.component';
+import { HomeActions } from './home/home.actions';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, TableComponent, TabsComponent, LandingComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [HomeActions],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
