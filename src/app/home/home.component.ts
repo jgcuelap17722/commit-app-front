c2a31369e020b8ce74953f05c3918a40e3777875
jose cuela <jgcuelap@gmail.com>
2023-10-12 04:49:43 -0500

    feature: add component home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { HomeActions } from './home.actions';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  listCommitBack: any[] = [];
 
+  constructor(
+    private apiService: ApiService,
+    private readonly _homeActions: HomeActions
+  ) {}
+
+  ngOnInit(): void {
+    this.getListCommitBack();
+    this.getListCommitFront();
+  }
+
+  getListCommitBack() {
+    this.apiService.getListCommitBack().subscribe((data) => {
+      this._homeActions.setListCommitBack(data);
+    });
+  }
+
+  getListCommitFront() {
+    this.apiService.getListCommitFront().subscribe((data) => {
+      this._homeActions.setListCommitFront(data);
+    });
+  }
 }
