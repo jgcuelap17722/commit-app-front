3887d83892c1e8cbacd50f3e12ee80e72ecd58e8
jose cuela <jgcuelap@gmail.com>
2023-10-12 04:46:51 -0500

    feature: add url list commit frontend

---
+++ src/app/urls.ts
@@ -0,0 +1,4 @@
+import { environment } from 'src/environments/environment';
+
+export const LIST_COMMITS_BACK = `${environment.API_URL}/commit/list-commit-back`;
+export const LIST_COMMITS_FRONT = `${environment.API_URL}/commit/list-commit-front`;
