9e306cb7dbc54d2728e745401ad513e38b1f9ea1
jose cuela <jgcuelap@gmail.com>
2023-10-13 12:14:45 -0500

    feature: update services with queries

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LIST_COMMITS_BACK, LIST_COMMITS_FRONT } from '../urls';
+import { apiResponse } from '../home/components/interfaces/list-commit.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,11 +10,25 @@ import { LIST_COMMITS_BACK, LIST_COMMITS_FRONT } from '../urls';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  public getListCommitBack(): Observable<any> {
-    return this.http.get<any>(LIST_COMMITS_BACK);
+  public getListCommitBack(
+    page: number,
+    limit: number = 10
+  ): Observable<apiResponse> {
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('limit', limit.toString());
+
+    return this.http.get<any>(LIST_COMMITS_BACK, { params });
   }
 
-  public getListCommitFront(): Observable<any> {
-    return this.http.get<any>(LIST_COMMITS_FRONT);
+  public getListCommitFront(
+    page: number,
+    limit: number = 10
+  ): Observable<apiResponse> {
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('limit', limit.toString());
+
+    return this.http.get<any>(LIST_COMMITS_FRONT, { params });
   }
 }
