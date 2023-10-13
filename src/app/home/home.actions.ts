715cbfca8b3f34f3a27764810559e2776c26c8ae
jose cuela <jgcuelap@gmail.com>
2023-10-13 12:07:56 -0500

    feature: implement interface in home actions

--- src/app/home/home.actions.ts
+++ src/app/home/home.actions.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { apiResponse } from './components/interfaces/list-commit.interface';
 
 @Injectable()
 export class HomeActions {
-  private _listCommitBack = new Subject<object[]>();
-  private _listCommitFront = new Subject<object[]>();
+  private _listCommitBack = new Subject<apiResponse>();
+  private _listCommitFront = new Subject<apiResponse>();
 
-  public getListCommitBack() {
+  public getListCommitBack(): Observable<apiResponse> {
     return this._listCommitBack.asObservable();
   }
-  public setListCommitBack(value: object[]) {
+  public setListCommitBack(value: apiResponse) {
     this._listCommitBack.next(value);
   }
 
-  public getListCommitFront() {
+  public getListCommitFront(): Observable<apiResponse> {
     return this._listCommitFront.asObservable();
   }
-  public setListCommitFront(value: object[]) {
+  public setListCommitFront(value: apiResponse) {
     this._listCommitFront.next(value);
   }
 }
